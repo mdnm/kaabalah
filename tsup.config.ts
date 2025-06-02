4f7bfa6cd97f52052856ca372df3e6387598de8f
Mateus Nardo <m.nardo@epilot.cloud>
2025-06-02 02:46:21 +0200

    v1.0.1

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: [
     'src/index.ts',
+    'src/core/index.ts',
     'src/numerology/index.ts',
     'src/astrology/index.ts',
-    'src/kaabalah/index.ts',
+    'src/gematria/index.ts',
+    'src/ifa/index.ts',
     'src/tarot/index.ts',
   ],
   format: ['cjs', 'esm'],
