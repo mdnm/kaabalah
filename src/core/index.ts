55bd9568fc03fac10c5b534e6bdad1d0fff86832
Mateus Nardo <m.nardo@epilot.cloud>
2025-06-06 22:42:09 +0200

    v2.0.1

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,4 @@
+export * from './constants';
 export * from './factory';
 export * from './systems/registry';
 export * from './tree-of-life';
--- src/tarot/index.ts
+++ src/tarot/index.ts
@@ -843,8 +843,9 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 const shuffleArray = <T,>(array: T[]): T[] => {
   const newArray = [...array]
   for (let i = newArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[newArray[i], newArray[j]] = [newArray[j], newArray[i]]
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
   }
   return newArray
 }
